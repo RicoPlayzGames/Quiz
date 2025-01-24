c48f0922ca44ac983e570f8d3af8db8cb356e77a
Tijn1806 <Tijn@kromjong.nl>
2025-01-24 10:21:36 +0100

    .css

--- php/login.php
+++ php/login.php
@@ -59,6 +59,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <a href="Home screen.php"><img src="Logo Quiz.png" alt="Logo quiz" class="logo"></a>
+    <link rel="stylesheet" href="login.css">
 </head>
 <body>
     <?php if ($error): ?>
