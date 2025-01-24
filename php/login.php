9efcee7201c4af8a6769c785e4e0929eae6dd99f
Jaiden Troenodikromo <6016497@mborijnland.nl>
2025-01-24 10:12:42 +0100

    login css

--- php/login.php
+++ php/login.php
@@ -55,6 +55,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="login.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
